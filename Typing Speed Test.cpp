3f26c7483c58ad291042ddee6548fc765f6c8fc1
Manijit Sau <manijit070@gmail.com>
2025-10-07 17:03:43 +0530

    Add files via upload

---
+++ Typing Speed Test.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <string>
+#include <ctime>
+using namespace std;
+
+int main() {
+
+
+    string challenge;
+    string input;
+    time_t start, end;
+    char choice;
+
+    cout << "******** WELCOME TO THE TYPING SPEED CHALLENGE! ********\n";
+    cout << "**** READY TO PLAY? (Y/N): ";
+    cin >> choice;
+    cin.ignore();
+
+    while (choice == 'Y' || choice == 'y') {
+        cout << "\nEnter the challenge line: ";
+        getline(cin, challenge);
+
+        cout << "\nType this line as fast as you can: " << challenge;
+        cout << "\nReady? Press Enter to start...";
+        cin.ignore();
+
+        start = time(nullptr);
+        getline(cin, input);
+        end = time(nullptr);
+
+        if (input == challenge)
+            cout << "CORRECT! Time taken: " << end - start << " seconds.\n";
+        else
+            cout << "INCORRECT! Try again!\n";
+
+        cout << "\nPlay again? (Y/N): ";
+        cin >> choice;
+        cin.ignore();
+    }
+
+        cout << "\n******** THANK YOU FOR PLAYING! ********\n";
+        return 0;
+    }
